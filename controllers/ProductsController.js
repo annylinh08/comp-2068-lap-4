ae93e727fbe9b783dd9ed79a6e83986e82697c95
Linh Duong <200393657@student.georgianc.on.ca>
2020-06-17 17:29:03 -0400

    Commit my lap 4

--- controllers/ProductsController.js
+++ controllers/ProductsController.js
@@ -1,7 +1,13 @@
 const viewPath = ('products');
 const Product = require('../models/product');
 
-exports.show = async (req, res) => {};
+exports.show = async (req, res) => {
+  const product = await Product.findById(req.params.id);
+  res.render(`${viewPath}/show`, {
+    pageTitle: product.name,
+    product: product
+  });
+};
 
 exports.new = (req, res) => {
   res.render(`${viewPath}/new`, {
@@ -9,4 +15,11 @@ exports.new = (req, res) => {
   });
 };
 
-exports.create = async (req, res) => {};
+exports.create = async (req, res) => {
+  try {
+    const product = await Product.create(req.body);
+    res.redirect(`/products/${product.id}`);
+  } catch (err) {
+    res.send(err);
+  }
+};
