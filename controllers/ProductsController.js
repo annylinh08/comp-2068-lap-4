2e9db326075d036b80c125fe4c32f0b2d7803064
Linh Duong <200393657@student.georgianc.on.ca>
2020-06-17 15:49:54 -0400

    my first commit for lap 4

---
+++ controllers/ProductsController.js
@@ -0,0 +1,12 @@
+const viewPath = ('products');
+const Product = require('../models/product');
+
+exports.show = async (req, res) => {};
+
+exports.new = (req, res) => {
+  res.render(`${viewPath}/new`, {
+    pageTitle: 'New Product'
+  });
+};
+
+exports.create = async (req, res) => {};
---
+++ models/product.js
@@ -0,0 +1,7 @@
+const mongoose = require('mongoose');
+
+// You need to create a new schema and assign it the following
+// constant
+const ProductSchema;
+
+module.exports = mongoose.model('Product', ProductSchema);
